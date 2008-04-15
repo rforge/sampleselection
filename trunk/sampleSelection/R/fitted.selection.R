e28d0f32968e83d4a63368c86bd99d2e73657cae
suapm095 <suapm095@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2008-04-15 09:28:16 +0000

    fitted.selection(): a few improvements and corrections in the code and in the documentation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@682 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- trunk/sampleSelection/R/fitted.selection.R
+++ trunk/sampleSelection/R/fitted.selection.R
@@ -10,21 +10,22 @@ fitted.selection <- function( object, part = "outcome", ... ) {
          result <- fitted( object$probit, ... )
       } else if( part == "outcome" ) {
          if( object$tobitType == 2 ) {
-            result <- fitted( object$lm )
+            result <- fitted( object$lm, ... )
          } else if( object$tobitType == 5 ) {
             response <- model.frame( object$probit )[ , 1 ]
             result <- rep( NA, length( response ) )
             result[ response == 0 ] <- fitted( object$lm1, ... )
             result[ response == 1 ] <- fitted( object$lm2, ... )
          } else {
-            stop( "unknown tobit type (object$tobitType)" )
+            stop( "unknown tobit type '",  object$tobitType,
+               "' in object$tobitType" )
          }
       } else {
          stop( "argument 'part' must be either 'outcome' or 'selection'" )
       }
    # maximum likelihood estimation
    } else if( object$method == "ml" ) {
-      stop( "the 'residuals' method has not yet been implemented for objects",
+      stop( "the 'fitted' method has not yet been implemented for objects",
          " estimated by Maximum Likelihood" )
    }
 

3cc54a1820248b7bfb55cb20bf1e4a35d10f1459
suapm095 <suapm095@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2008-04-17 07:50:20 +0000

    residuals.probit() can calculate also "deviance" residuals now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@694 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- trunk/sampleSelection/R/residuals.probit.R
+++ trunk/sampleSelection/R/residuals.probit.R
@@ -4,9 +4,12 @@ residuals.probit <- function( object, type = "response", ... ) {
    response <- model.frame( object )[ , 1 ]
    if( type == "response" ) {
       result <- response - fitVal
+   } else if( type == "deviance" ) {
+      result <- ifelse( response == 1,
+         sqrt( -2 * log( fitVal ) ), -sqrt( -2 * log( 1 - fitVal ) ) )
    } else if( type == "pearson" ) {
       result <- ( response - fitVal ) / sqrt( fitVal * ( 1 - fitVal ) )
-   } else if( type %in% c( "deviance", "working", "partial" ) ) {
+   } else if( type %in% c( "working", "partial" ) ) {
       stop( "type '", type, "' has not yet been implemented" )
    } else {
       stop( "argument 'type' must be either 'deviance', 'pearson',",

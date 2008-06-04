61039339d5d24fdd57704f8a92d7f4a97b9e2c02
suapm095 <suapm095@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2008-06-04 06:43:15 +0000

    the "fitted" method for objects of class "selection" works also for models estimated by the ML method now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@759 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- trunk/sampleSelection/R/fitted.selection.R
+++ trunk/sampleSelection/R/fitted.selection.R
@@ -26,9 +26,30 @@ fitted.selection <- function( object, part = "outcome", ... ) {
       }
    # maximum likelihood estimation
    } else if( object$method == "ml" ) {
-      stop( "the 'fitted' method has not yet been implemented for objects",
-         " estimated by Maximum Likelihood" )
+      if( part == "selection" ) {
+         result <- drop( pnorm( model.matrix( object, part = "selection" ) %*%
+            coef( object )[ object$param$index$betaS ] ) )
+      } else if( part == "outcome" ) {
+         if( object$tobitType == 2 ) {
+            result <- drop( model.matrix( object ) %*%
+               coef( object )[ object$param$index$betaO ] )
+         } else if( object$tobitType == 5 ) {
+            mm <- model.matrix( object )
+            response <- model.frame( object )[ , 1 ]
+            result <- rep( NA, length( response ) )
+            fitted1 <- mm[[ 1 ]] %*%
+               coef( object )[ object$param$index$betaO1 ]
+            fitted2 <- mm[[ 2 ]] %*%
+               coef( object )[ object$param$index$betaO2 ]
+            result[ response == 0 ] <- fitted1[ response == 0 ]
+            result[ response == 1 ] <- fitted2[ response == 1 ]
+         } else {
+            stop( "unknown tobit type '",  object$tobitType,
+               "' in object$tobitType" )
+         } 
+      } else {
+         stop( "argument 'part' must be either 'outcome' or 'selection'" )
+      }
    }
-
    return( result )
 }

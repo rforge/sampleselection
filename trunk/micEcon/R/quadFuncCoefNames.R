c1f72d2e9649184773c8fc3d81f64edd092d3ce1
suapm095 <suapm095@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2008-09-23 12:27:49 +0000

    removed /trunk (including sampleSelection_all.R and micEcon)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@996 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- trunk/micEcon/R/quadFuncCoefNames.R
+++
@@ -1,9 +0,0 @@
-.quadFuncCoefNames <- function( nExog ) {
-   result <- paste( "alpha", c( 0:nExog ), sep = "_" )
-   for( i in 1:nExog ) {
-      for( j in i:nExog ) {
-         result <- c( result, paste( "beta", i, j, sep = "_" ) )
-      }
-   }
-   return( result )
-}

e8fbb942ad7a0fcb17b6637d968bd225738d195b
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2009-07-13 14:38:08 +0000

    fixed bug: added "maxLik:::" before calls to "coef.maxLik", "summary.maxLik", and "vcov.maxLik" (this became necessary because a NAMESPACE file has been added in the maxLik package)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1016 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/R/summary.selection.R
+++ pkg/R/summary.selection.R
@@ -2,7 +2,7 @@ summary.selection <- function(object, ...) {
    ## object      object of class "selection"
 
    if( object$method == "ml" ) {
-      s <- summary.maxLik(object, ...)
+      s <- maxLik:::summary.maxLik(object, ...)
    } else if( object$method == "2step" )  {
       s <- list()  # list for results that will be returned
       RSq <- function(model, intercept) {

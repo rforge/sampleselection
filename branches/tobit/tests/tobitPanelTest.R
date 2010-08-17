1c67c10a1065711968d4556f1eb17ea9d0c3fb9b
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2010-08-17 12:52:08 +0000

    added test for random-effects panel-data tobit model with a modified order of individuals
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1079 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- branches/tobit/tests/tobitPanelTest.R
+++ branches/tobit/tests/tobitPanelTest.R
@@ -13,6 +13,7 @@ pData$x1 <- rnorm( nId * nTime )
 pData$x2 <- runif( nId * nTime )
 pData$ys <- -1 + pData$ui + 2 * pData$x1 + 3 * pData$x2 + rnorm( nId * nTime )
 pData$y <- ifelse( pData$ys > 0, pData$ys, 0 )
+nData <- pData # save data set without information on panel structure
 pData <- pdata.frame( pData, c( "id", "time" ) )
 
 
@@ -38,3 +39,18 @@ system.time( randEffBfgsr <- tobit( y ~ x1 + x2, data = pData,
    method = "BFGSR" ) )
 summary( randEffBfgsr )
 
+
+## re-order observations/individuals
+set.seed( 234 )
+perm <- sample( nId )
+nData2 <- nData
+nData2$id <- NA
+for( i in 1:nId ) {
+   nData2$id[ nData$id == paste( "F", i, sep = "_" ) ] <-
+      paste( "G", perm[ i ], sep = "_" )
+}
+pData2 <- pdata.frame( nData2, c( "id", "time" ) )
+system.time( randEffBfgsr2 <- tobit( y ~ x1 + x2, data = pData2,
+   method = "BFGSR" ) )
+all.equal( randEffBfgsr2[ -11 ], randEffBfgsr[ -11 ] )
+all.equal( sort( randEffBfgsr2[[ 11 ]] ), sort( randEffBfgsr[[ 11 ]] ) )

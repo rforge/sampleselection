18c737615da2653f32e5854d7d627788f6e4904b
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2010-08-18 09:49:57 +0000

    the (internal) function that calculates the log-likelihood value of the random-effects panel data tobit model now additionally returns the gradients of the log-likelihood function with respect to the coefficients; this reduces the execution time for the models in /tests/tobitPanel[Larger]Test.R by a factor between 2 and 4
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1089 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- branches/tobit/R/tobit.R
+++ branches/tobit/R/tobit.R
@@ -132,13 +132,14 @@ tobit <- function( formula, left = 0, right = Inf,
       obsMatAbove <- yMat >= right & !is.na( yMat )
       obsMatBetween <- !obsMatBelow & !obsMatAbove & !is.na( yMat )
 
-      ## log likelihood function for panel data
+      ## log likelihood function for panel data (incl. gradients)
       tobitLogLik <- function( beta ) {
          yMatHat <- matrix( matrix( xArr, ncol = ncol( xMat ) ) %*%
             beta[ 1:( length( beta ) - 2 ) ], nrow = nInd, ncol = nTime )
          sigmaMu <- exp( beta[ length( beta ) - 1 ] )
          sigmaNu <- exp( beta[ length( beta ) ] )
          likInd <- rep( 0, nInd )
+         gradInd <- matrix( 0, nrow = nInd, ncol = length( beta ) )
          for( h in 1:nGHQ ) {
             likGhqInner <- matrix( NA, nrow = nInd, ncol = nTime )
             likGhqInner[ obsMatBelow ] <-
@@ -157,8 +158,37 @@ tobit <- function( formula, left = 0, right = Inf,
                dnorm( likGhqInner[ obsMatBetween ] ) / sigmaNu
             likGhqProd <- apply( likGhq, 1, prod )
             likInd <- likInd + ghqPoints$weights[ h ] * likGhqProd
+            # gradients
+            gradPartGhq <- matrix( 0, nrow = nInd, ncol = nTime )
+            gradPartGhq[ obsMatBelow ] <-
+               - dnorm( likGhqInner[ obsMatBelow ] ) / sigmaNu
+            gradPartGhq[ obsMatAbove ] <-
+               dnorm( likGhqInner[ obsMatAbove ] ) / sigmaNu
+            gradPartGhq[ obsMatBetween ] <-
+               - ddnorm( likGhqInner[ obsMatBetween ] ) / sigmaNu^2
+            # part of gradients with respect to beta
+            for( i in 1:( length( beta ) - 2 ) ) {
+               gradInd[ , i ] <- gradInd[ , i ] + ghqPoints$weights[ h ] *
+                  likGhqProd * rowSums( gradPartGhq * xArr[ , , i ] / likGhq,
+                  na.rm = TRUE )
+            }
+            # part of gradient with respect to log( sigma_mu )
+            gradInd[ , length( beta ) - 1 ] <- gradInd[ , length( beta ) - 1 ] +
+               sigmaMu * ghqPoints$weights[ h ] * likGhqProd *
+               rowSums( gradPartGhq * sqrt( 2 ) * ghqPoints$zeros[ h ] / likGhq )
+            # part of gradient with respect to log( sigma_nu )
+            gradPartGhq[ obsMatBelow ] <- gradPartGhq[ obsMatBelow ] *
+               likGhqInner[ obsMatBelow ]
+            gradPartGhq[ obsMatAbove ] <- - gradPartGhq[ obsMatAbove ] *
+               likGhqInner[ obsMatAbove ]
+            gradPartGhq[ obsMatBetween ] <- gradPartGhq[ obsMatBetween ] *
+               likGhqInner[ obsMatBetween ] - likGhq[ obsMatBetween ] / sigmaNu
+            gradInd[ , length( beta ) ] <- gradInd[ , length( beta ) ] +
+               sigmaNu * ghqPoints$weights[ h ] * likGhqProd *
+               rowSums( gradPartGhq / likGhq )
          }
          ll <- log( likInd / sqrt( pi ) )
+         attr( ll, "gradient" ) <- gradInd / likInd
          return( ll )
       }
    } else {
@@ -203,7 +233,6 @@ tobit <- function( formula, left = 0, right = Inf,
          return( ll )
       }
    }
-
    result <- maxLik( tobitLogLik, start = start, ... )
 
    class( result ) <- c( "tobit", class( result ) )

bc7afd94936c18c21df6b9a7c845e074ffe4f48e
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2010-08-19 21:33:52 +0000

    using deparse() in print.censReg() for printing the call now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1118 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- branches/tobit/R/print.censReg.R
+++ branches/tobit/R/print.censReg.R
@@ -2,8 +2,8 @@ print.censReg <- function( x, logSigma = TRUE, digits = 4, ... ) {
 
    cat( "\n" )
    cat( "Call:\n" )
-   print( x$call )
-   cat( "\n" )
+   cat( paste( deparse( x$call ), sep = "\n", collapse = "\n" ) )
+   cat( "\n\n" )
    cat( "Coefficients:\n" )
    print( coef( x, logSigma = logSigma ), digits = digits )
    cat( "\n" )

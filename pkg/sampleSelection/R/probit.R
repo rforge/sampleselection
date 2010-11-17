6370c158a9def220781eef974c081a2b60d8f017
otoomet <otoomet@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2010-11-17 02:58:09 +0000

    probit.R made more modular in order to generalise for logit & friends
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1142 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/sampleSelection/R/probit.R
+++ pkg/sampleSelection/R/probit.R
@@ -25,30 +25,77 @@ probit <- function(formula, subset, na.action,
    ##  x         model matrix (only if requested)
    ##  call      call
    ##  terms     terms
-   loglik <- function( beta) {
-      xb0 <- x0 %*% beta
-      xb1 <- x1 %*% beta
-      loglik <- sum(pnorm( xb0, log=TRUE, lower.tail=FALSE)) + sum(pnorm( xb1, log.p=TRUE))
+   loglik <- function(beta) {
+      ## A generic linear index parametric binary choice log-likelihood:
+      ## F: disturbance terms cdf
+      ## f:                   pdf
+      ## 
+      ## l = sum(log(1 - F(x0'beta))) + sum(log(F(x1'beta)))
+      ## dl/dbeta = sum(-f(x0'beta)/(1 - F(x0'beta)) * x0) + sum(f(x1'beta)/F(x1'beta) * x1)
+      ## d2l/dbeta2 =
+      ##      sum((-df(x0'beta)/dbeta*(1 - F(x0'beta)) - f^2(x0'beta))/(1 - F(x0'beta))^2 *x0'x0 +
+      ##      sum((df(x1'beta)/dbeta*F(x1'beta) - f^2(x1'beta))/F(x1'beta)^2 *x1'x1
+      ##
+      ## We need to supply 4 specific functions:
+      ## F          cdfLower    (pnorm for probit)
+      ## 1 - F      cdfUpper    (pnorm(.., lower.tail=FALSE))
+      ## f          pdf         (dnorm)
+      ## df/dbeta   gradPdf     (-dnorm(x)*x)
+      xb0 <- drop(x0 %*% beta)
+      xb1 <- drop(x1 %*% beta)
+      #
+      F0 <- cdfUpper(xb0)
+      F1 <- cdfLower(xb1)
+      loglik <- numeric(length(Y))
+      loglik[Y == 0] <- log(F0)
+      loglik[Y == 1] <- log(F1)
+      ##
+      f0 <- pdf(xb0)
+      f1 <- pdf(xb1)
+      gradlik <- matrix(0, length(Y), length(beta))
+      gradlik[Y == 0,] <- - pdf(xb0)/F0 * x0
+      gradlik[Y == 1,] <- pdf(xb1)/F1 * x1
+      ##
+      gradf0 <- gradPdf(xb0)
+      gradf1 <- gradPdf(xb1)
+      hesslik <-
+          t( x0) %*% ( x0 * ( -gradf0*F0 - f0*f0)/F0/F0) +
+              t( x1) %*% ( x1 * ( gradf1*F1 - f1*f1)/F1/F1)
+      attr(loglik, "gradient") <- gradlik
+      attr(loglik, "hessian") <- hesslik
+      loglik
    }
-   gradlik <- function(beta) {
-      ## gradient is 1 x nParam matrix
-      xb0 <- x0 %*% beta
-      xb1 <- x1 %*% beta
-      gradlik <- - t(dnorm(xb0)/pnorm(xb0, lower.tail=FALSE)) %*% x0 +
-          t(dnorm(xb1)/pnorm(xb1)) %*% x1
-   }
-   hesslik <- function(beta) {
-      xb0 <- as.vector( x0 %*% beta)
-      xb1 <- as.vector( x1 %*% beta)
-      F0 <- pnorm( xb0)
-      F1 <- pnorm( xb1)
-      f0 <- dnorm( xb0)
-      f1 <- dnorm( xb1)
-      yF0 <- pnorm( xb0, lower.tail=FALSE)
-      loglik2 <- t( x1) %*% ( x1 * ( -f1*xb1*F1 - f1*f1)/F1/F1) -
-         t( x0) %*% ( x0 * ( -f0*xb0*yF0 + f0*f0)/yF0/yF0)
-                    # note that df/db' = -f (x'b) x'
-   }
+   ## loglik <- function( beta) {
+   ##    xb0 <- x0 %*% beta
+   ##    xb1 <- x1 %*% beta
+   ##    loglik <- sum(pnorm( xb0, log=TRUE, lower.tail=FALSE)) + sum(pnorm( xb1, log.p=TRUE))
+   ## }
+   ## gradlik <- function(beta) {
+   ##    ## gradient is 1 x nParam matrix
+   ##    xb0 <- x0 %*% beta
+   ##    xb1 <- x1 %*% beta
+   ##    gradlik <- - t(dnorm(xb0)/pnorm(xb0, lower.tail=FALSE)) %*% x0 +
+   ##        t(dnorm(xb1)/pnorm(xb1)) %*% x1
+   ## }
+   ## hesslik <- function(beta) {
+   ##    xb0 <- as.vector( x0 %*% beta)
+   ##    xb1 <- as.vector( x1 %*% beta)
+   ##    F0 <- pnorm( xb0)
+   ##    F1 <- pnorm( xb1)
+   ##    f0 <- dnorm( xb0)
+   ##    f1 <- dnorm( xb1)
+   ##    yF0 <- pnorm( xb0, lower.tail=FALSE)
+   ##    loglik2 <- t( x1) %*% ( x1 * ( -f1*xb1*F1 - f1*f1)/F1/F1) -
+   ##       t( x0) %*% ( x0 * ( -f0*xb0*yF0 + f0*f0)/yF0/yF0)
+   ##                  # note that df/db' = -f (x'b) x'b x'
+   ## }
+   ## 
+   ## probit specific stuff
+   cdfLower <- pnorm
+   cdfUpper <- function(x) pnorm(x, lower.tail=FALSE)
+   pdf <- dnorm
+   gradPdf <- function(x) -dnorm(x)*x
+   ## Binary choice specific stuff
    cl <- match.call()
    mf <- match.call(expand.dots = FALSE)
    m <- match(c("formula", "data", "subset", "weights", "na.action",
@@ -91,15 +138,15 @@ probit <- function(formula, subset, na.action,
       names(start) <- dimnames(X)[[2]]
    }
    ## Main estimation
-   estimation <- maxLik(loglik, gradlik, hesslik, start,
+   estimation <- maxLik(loglik, start=start,
                         method="Newton-Raphson", ...)
    ## compare.derivatives(gradlik, hesslik, t0=start)
                                         #
    ## Likelihood ratio test: H0 -- all the coefficients, except intercept
    ## are zeros.  ML estimate for this model is qnorm(N1/nObs)
-   ll.bar <- loglik(c(qnorm(N1/nObs), rep(0, nParam-1)))
-   LRT <- 2*(estimation$maximum - ll.bar)
-                                        #
+   ll.bar <- sum(loglik(c(qnorm(N1/nObs), rep(0, nParam-1))))
+   LRT <- 2*(logLik(estimation) - ll.bar)
+                                        # note: this assumes constant is in the first column
    result <- c(estimation,
                LRT=list(list(LRT=LRT, df=nParam-1)),
                                         # there are df-1 constraints

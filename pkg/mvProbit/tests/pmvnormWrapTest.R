426eb46afa55d9f3923f18bc8077614209d3b46a
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2011-09-27 13:32:55 +0000

    added test script and corresponding output file for pmvnormWrap()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1224 d36ae590-6c38-44c5-9ba5-7217e93607d8

---
+++ pkg/mvProbit/tests/pmvnormWrapTest.R
@@ -0,0 +1,74 @@
+library( "mvProbit" )
+library( "miscTools" )
+
+# covariance matrix
+sigma <- symMatrix( c( 1, 0.2, 0.4, 1, -0.1, 1 ) )
+
+######## only upper ##########
+upper <- c( -0.3, 0.7, -0.5 )
+# Genz + Bretz (default)
+pug <- mvProbit:::pmvnormWrap( upper = upper, sigma = sigma, 
+   algorithm = GenzBretz(), random.seed = 123 )
+print( pug )
+
+# Miwa (as function)
+pum <- mvProbit:::pmvnormWrap( upper = upper, sigma = sigma, 
+   algorithm = Miwa, random.seed = 123 )
+print( pum )
+all.equal( pug, pum, check.attributes = FALSE )
+
+# Miwa (as object returned from function Miwa())
+pum1 <- mvProbit:::pmvnormWrap( upper = upper, sigma = sigma, 
+   algorithm = Miwa(), random.seed = 123 )
+all.equal( pum, pum1 )
+
+# Miwa (as character string)
+pum2 <- mvProbit:::pmvnormWrap( upper = upper, sigma = sigma, 
+   algorithm = "Miwa", random.seed = 123 )
+all.equal( pum, pum2 )
+
+# TVPACK
+put <- mvProbit:::pmvnormWrap( upper = upper, sigma = sigma, 
+   algorithm = TVPACK, random.seed = 123 )
+print( put )
+all.equal( pug, put, check.attributes = FALSE )
+all.equal( pum, put, check.attributes = FALSE )
+
+
+######## only lower ##########
+lower <- c( -0.7, 0.3, -0.9 )
+# Genz + Bretz (default)
+plg <- mvProbit:::pmvnormWrap( lower = lower, sigma = sigma, 
+   algorithm = GenzBretz, random.seed = 123 )
+print( plg )
+
+# Miwa
+plm <- mvProbit:::pmvnormWrap( lower = lower, sigma = sigma, 
+   algorithm = Miwa, random.seed = 123 )
+print( plm )
+all.equal( plg, plm, check.attributes = FALSE )
+
+# TVPACK
+plt <- mvProbit:::pmvnormWrap( lower = lower, sigma = sigma, 
+   algorithm = TVPACK, random.seed = 123 )
+print( plt )
+all.equal( plg, plt, check.attributes = FALSE )
+all.equal( plm, plt, check.attributes = FALSE, tolerance = 1e-10 )
+
+
+######## partly lower, partly upper ##########
+lower2 <- c( -Inf, 0.3, -Inf )
+upper2 <- c( -0.3, Inf, -0.5 )
+# Genz + Bretz (default)
+pbg <- mvProbit:::pmvnormWrap( lower = lower2, upper = upper2, sigma = sigma, 
+   algorithm = GenzBretz(), random.seed = 123 )
+print( pbg )
+
+# Miwa
+pbm <- mvProbit:::pmvnormWrap( lower = lower2, upper = upper2, sigma = sigma, 
+   algorithm = Miwa, random.seed = 123 )
+print( pbm )
+all.equal( pbg, pbm, check.attributes = FALSE )
+
+
+

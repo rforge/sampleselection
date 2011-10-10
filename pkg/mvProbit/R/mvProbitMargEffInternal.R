48547701ce75b4cdd74d52598ee838e6dd89db3c
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2011-10-10 14:01:58 +0000

    moved a large part of the code from mvProbitMargEff() into the new internal function mvProbitMargEffInternal() that will later also be used to obtain the Jacobian of the marginal effects with respect to the coefficients; the codel for preparing the response matrix and the model matrix remained in mvProbitMargEff()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1270 d36ae590-6c38-44c5-9ba5-7217e93607d8

---
+++ pkg/mvProbit/R/mvProbitMargEffInternal.R
@@ -0,0 +1,55 @@
+mvProbitMargEffInternal <- function( yMat, xMat, coef, sigma, data,
+   cond, algorithm, nGHK, eps, 
+   random.seed, ... ) {
+
+   # number of regressors
+   nReg <- ncol( xMat )
+
+   # names of regressors
+   xNames <- colnames( xMat )
+
+   # calculate marginal effects
+   for( i in 2:nReg ) {
+      xMatL <- xMatU <- xMat
+      isDummy <- all( xMat[ , i ] %in% c( 0, 1, FALSE, TRUE ) )
+      if( isDummy ) {
+         xMatL[ , i ] <- 0
+         xMatU[ , i ] <- 1
+      } else {
+         xMatL[ , i ] <- xMat[ , i ] - eps / 2
+         xMatU[ , i ] <- xMat[ , i ] + eps / 2
+      }
+      yMatL <- mvProbitExpInternal( yMat = yMat, xMat = xMatL,
+         coef = coef, sigma = sigma, 
+         cond = cond, algorithm = algorithm, nGHK = nGHK, 
+         random.seed = random.seed, ... )
+      yMatU <- mvProbitExpInternal( yMat = yMat, xMat = xMatU, 
+         coef = coef, sigma = sigma, 
+         cond = cond, algorithm = algorithm, nGHK = nGHK, 
+         random.seed = random.seed, ... )
+      margEff <- yMatU - yMatL
+      if( !isDummy ) {
+         margEff <- margEff / eps
+      }
+
+      # names of dependent variables
+      if( i == 2 ) {
+         if( !is.null( yMat ) ) {
+            yNames <- colnames( yMat )
+         } else {
+            yNames <- paste( "y", 1:ncol( margEff ), sep = "" )
+         }
+      }
+
+      # label marginal effects
+      names( margEff ) <- paste( "d", yNames, "d", xNames[ i ], sep = "_" )
+
+      if( i == 2 ) {
+         result <- margEff
+      } else {
+         result <- cbind( result, margEff )
+      }
+   }
+
+   return( result )
+}

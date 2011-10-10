7eca67b8b7235f0152f72bcfdc6f1115432f4175
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2011-10-10 08:10:23 +0000

    moved code for checking and preparing coefficients from mvProbitExpInternal() into a new internal function mvProbitPrepareCoef()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1254 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/mvProbit/R/mvProbitExpInternal.R
+++ pkg/mvProbit/R/mvProbitExpInternal.R
@@ -14,79 +14,16 @@ mvProbitExpInternal <- function( yMat, xMat, coef, sigma,
    # number of observations
    nObs <- nrow( xMat )
 
-   # checking argument 'coef'
-   if( !is.vector( coef, mode = "numeric" ) ) {
-      stop( "argument 'coef' must be a numeric vector" )
-   }
+   coef <- mvProbitPrepareCoef( yMat = yMat, nReg = nReg, coef = coef, 
+      sigma = sigma )
 
-   if( !is.null( sigma ) ) {
-      # checking argument 'sigma'
-      if( !is.matrix( sigma ) ) {
-         stop( "argument 'sigma' must be a matrix" )
-      } else if( nrow( sigma ) != ncol( sigma ) ) {
-         stop( "argument 'sigma' must be a quadratic matrix" )
-      } else if( !isSymmetric( sigma ) ) {
-         stop( "argument 'sigma' must be a symmetric matrix" )
-      } else if( any( abs( diag( sigma ) - 1 ) > 1e-7 ) ) {
-         stop( "argument 'sigma' must have ones on its diagonal" )
-      } else if( !is.null( yMat ) ) {
-         if( ncol( sigma ) != ncol( yMat ) ) {
-            stop( "the number of dependent variables specified in argument",
-               " 'formula' must be equal to the number of rows and colums",
-               " of the matrix specified by argument 'sigma'" )
-         }
-      }
-      # number of dependent variables
-      nDep <- ncol( sigma )
-      # number of coefficients
-      nCoef <- nDep * nReg
-      if( length( coef ) != nCoef ) {
-         stop( "given that argument 'sigma' has been specified",
-            " argument coef must have ", nCoef, " elements" )
-      }
-   } else {
-      if( !is.null( yMat ) ) {
-         # number of dependent variables
-         nDep <- ncol( yMat )
-         # number of coefficients
-         nCoef <- nDep * nReg
-         # number of parameters including sigma
-         nCoefSigma <- nCoef + nDep * ( nDep - 1 ) / 2
-         if( length( coef ) != nCoefSigma ) {
-            stop( "given that argument 'sigma' is 'NULL'",
-               " argument coef must have ", nCoefSigma, " elements" )
-         }
-      } else {
-         # number of dependent variables
-         nDep <- round( - nReg + 0.5 + 
-            sqrt( ( nReg - 0.5 )^2 + 2 * length( coef ) ) )
-         # number of coefficients
-         nCoef <- nDep * nReg
-         # number of parameters including sigma
-         nCoefSigma <- nCoef + nDep * ( nDep - 1 ) / 2
-         if( length( coef ) != nCoefSigma ) {
-            stop( "given that argument 'sigma' is 'NULL'",
-               " argument coef must have ", nCoefSigma, " elements",
-               " if the model has ", nDep, " dependent variables" )
-         }
-      }
-      # extracting correlation coefficients from 'coef' if they are there
-      sigma <- diag( nDep )
-      sigma[ lower.tri( sigma ) ] <- coef[ -c( 1:nCoef ) ]
-      sigma[ upper.tri( sigma ) ] <- t( sigma )[ upper.tri( sigma ) ]
-      coef <- coef[ 1:nCoef ]
-   }
-
-   # separating coefficients for different equations
-   betaEq <- list()
-   for( i in 1:nDep ) {
-      betaEq[[ i ]] <- coef[ ( ( i - 1 ) * nReg + 1 ):( i * nReg ) ]
-   }
+   # number of dependent variables
+   nDep <- ncol( coef$sigma )
 
    # calculating linear predictors
    xBeta <- matrix( NA, nrow = nObs, ncol = nDep )
    for( i in 1:nDep ) {
-      xBeta[ , i ] <- xMat %*% betaEq[[ i ]]
+      xBeta[ , i ] <- xMat %*% coef$betaEq[[ i ]]
    }
 
    if( cond ) {
@@ -97,10 +34,10 @@ mvProbitExpInternal <- function( yMat, xMat, coef, sigma,
          for( i in 1:nObs ) {
             for( k in 1:nDep ) {
                result[ i, k ] <- 
-                  pmvnormWrap( upper = xBeta[ i, ], sigma = sigma,
+                  pmvnormWrap( upper = xBeta[ i, ], sigma = coef$sigma,
                      algorithm = algorithm, nGHK = nGHK, 
                      random.seed = random.seed, ... ) /
-                  pmvnormWrap( upper = xBeta[ i, -k ], sigma = sigma[ -k, -k ],
+                  pmvnormWrap( upper = xBeta[ i, -k ], sigma = coef$sigma[ -k, -k ],
                      algorithm = algorithm, nGHK = nGHK, 
                      random.seed = random.seed, ... )
             }
@@ -112,7 +49,7 @@ mvProbitExpInternal <- function( yMat, xMat, coef, sigma,
                ySign <- 2 * yMat[ i, ] - 1
                ySign[ k ] <- 1
                xBetaTmp <- xBeta[ i, ] * ySign
-               sigmaTmp <- diag( ySign ) %*% sigma %*% diag( ySign )
+               sigmaTmp <- diag( ySign ) %*% coef$sigma %*% diag( ySign )
                result[ i, k ] <- 
                   pmvnormWrap( upper = xBetaTmp, sigma = sigmaTmp,
                      algorithm = algorithm, nGHK = nGHK, 

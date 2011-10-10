a0df495b51ddff855aab0a41f15c1bae708c51c1
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2011-10-10 09:34:23 +0000

    simplified code in margEff.mvProbit() by specifying just argument "coef" (and no longer argument "sigma") of mxProbitMargEff()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1259 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/mvProbit/R/margEff.mvProbit.R
+++ pkg/mvProbit/R/margEff.mvProbit.R
@@ -1,19 +1,13 @@
 margEff.mvProbit <- function( object, data = eval( object$call$data ),
    othDepOne = FALSE, ... ) {
 
-   nCoef <- object$nDep * object$nReg
-
-   sigma <- diag( object$nDep )
-   sigma[ lower.tri( sigma ) ] <- coef( object )[ -(1:nCoef) ]
-   sigma[ upper.tri( sigma ) ] <- t( sigma )[ upper.tri( sigma ) ]
-
    formula <- eval( object$call$formula )
    if( othDepOne ) {
       formula <- formula[ - 2 ]
    }
 
    result <- mvProbitMargEff( formula = formula, 
-      coef = coef( object )[ 1:nCoef ], sigma = sigma, data = data, ... )
+      coef = coef( object ), data = data, ... )
 
    return( result )
 }

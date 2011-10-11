cce03742a4fbf3190dc1d1efd36223c082670433
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2011-10-11 12:56:28 +0000

    added a summary() method for objects of class "mvProbitMargEff"; added a print() method for objects of class "summary.mvProbitMargEff"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1281 d36ae590-6c38-44c5-9ba5-7217e93607d8

---
+++ pkg/mvProbit/R/summary.mvProbitMargEff.R
@@ -0,0 +1,28 @@
+summary.mvProbitMargEff <- function( object, ... ) {
+
+   nObs <- nrow( object )
+   nMargEff <- ncol( object )
+
+   result <- matrix( NA, nrow = nObs * nMargEff, ncol = 4 )
+   rownames( result ) <- paste(
+      rep( rownames( object ), each = ncol( object ) ), ": ", 
+      rep( colnames( object ), nrow( object ) ), sep = "" )
+   colnames( result ) <- c( "Estimate", "Std. error", "z value", "Pr(> z)" )
+
+   result[ , 1 ] <- c( t( as.matrix( object ) ) )
+
+   margEffVCov <- attr( object, "vcov" )
+   if( !is.null( margEffVCov ) ) {
+      for( i in 1:nObs ) {
+         result[ ( (i-1) * nMargEff + 1 ):( i * nMargEff ), 2 ] <-
+            sqrt( diag( margEffVCov[ i, , ] ) )
+      }
+      result[ , 3 ] <- result[ , 1 ] / result[ , 2 ]
+      result[ , 4 ] <- 2 * pnorm( -abs( result[ , 3 ] ) )
+   }
+
+   class( result ) <- c( "summary.mvProbitMargEff", class( result ) )
+
+   return( result )
+}
+

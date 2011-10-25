262eb5202c09b170b50c7e04f6a8a02cd551baca
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2011-10-25 11:28:34 +0000

    the margEff() method now checks whether arguments "data", "cond", "othDepOne", and "calcVCov" are of the expected type
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1305 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/mvProbit/R/margEff.mvProbit.R
+++ pkg/mvProbit/R/margEff.mvProbit.R
@@ -2,6 +2,32 @@ margEff.mvProbit <- function( object, data = eval( object$call$data ),
    cond = FALSE, othDepOne = FALSE, dummyVars = object$dummyVars,
    calcVCov = FALSE,... ) {
 
+   # checking argument 'data'
+   if( !is.data.frame( data ) ) {
+      stop( "argument 'data' must be a 'data.frame'" )
+   }
+
+   # checking argument 'cond'
+   if( length( cond ) != 1 ) {
+      stop( "argument 'cond' must be a single logical value" )
+   } else if( !is.logical( cond ) ) {
+      stop( "argument 'cond' must be a logical value" )
+   }
+
+   # checking argument 'othDepOne'
+   if( length( othDepOne ) != 1 ) {
+      stop( "argument 'othDepOne' must be a single logical value" )
+   } else if( !is.logical( othDepOne ) ) {
+      stop( "argument 'othDepOne' must be a logical value" )
+   }
+
+   # checking argument 'calcVCov'
+   if( length( calcVCov ) != 1 ) {
+      stop( "argument 'calcVCov' must be a single logical value" )
+   } else if( !is.logical( calcVCov ) ) {
+      stop( "argument 'calcVCov' must be a logical value" )
+   }
+
    formula <- eval( object$call$formula )
    if( othDepOne || !cond ) {
       formula <- formula[ - 2 ]

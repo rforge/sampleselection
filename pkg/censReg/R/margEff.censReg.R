a842282f03676f849b5847e7190b16ea8ae40ce9
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2012-04-11 20:10:31 +0000

    margEff.censReg() now returns an error message rather than NULL, if the model was estimated as panel data model
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1390 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/censReg/R/margEff.censReg.R
+++ pkg/censReg/R/margEff.censReg.R
@@ -7,54 +7,56 @@ margEff.censReg <- function( object, calcVCov = TRUE, returnJacobian = FALSE,
    isPanel <- "sigmaMu" %in% names( allPar )
    
    ## (not for panel data)
-   if( !isPanel ) {
-      sigma <- allPar[ "sigma" ]
-      beta <- allPar[ ! names( allPar ) %in% c( "sigma" ) ]
-      if( length( object$xMean ) != length( beta ) ){
-         print( beta )
-         print( object$xMean )
-         stop( "cannot calculate marginal effects due to an internal error:",
-                  " please contact the maintainer of this package" )
-      }
-      xBeta <- crossprod( object$xMean, beta )
-      zRight <- ( object$right - xBeta ) / sigma
-      zLeft <- ( object$left - xBeta ) / sigma
-      result <- beta[ ! names( beta ) %in% c( "(Intercept)" ) ] * 
-         ( pnorm( zRight ) - pnorm( zLeft ) )
-      names( result ) <- 
-         names( beta )[ ! names( beta ) %in% c( "(Intercept)" ) ]
-
-      if( calcVCov ){
-         # compute Jacobian matrix
-         jac <- matrix( 0, nrow = length( result ), ncol = length( allPar ) )
-         rownames( jac ) <- names( result )
-         colnames( jac ) <- names( allPar )
-         for( j in names( result ) ) {
-            for( k in names( allPar )[ -length( allPar ) ] ) {
-               jac[ j, k ] <- 
-                  ( j == k ) * ( pnorm( zRight ) - pnorm( zLeft ) ) -
-                  ( beta[ j ] * object$xMean[ j ] / sigma ) *
-                  ( dnorm( zRight ) - dnorm( zLeft ) )
-            }
-            jac[ j, "sigma"] <- 0
-            if( is.finite( object$right ) ) {
-               jac[ j, "sigma"] <- jac[ j, "sigma"] - ( beta[ j ] / sigma ) *
-                  dnorm( zRight ) * zRight
-            }
-            if( is.finite( object$left ) ) {
-               jac[ j, "sigma"] <- jac[ j, "sigma"] + ( beta[ j ] / sigma ) *
-                  dnorm( zLeft ) * zLeft
-            }
-         }
-         attr( result, "vcov" ) <- 
-            jac %*% vcov( object, logSigma = FALSE ) %*% t( jac )
-         if( returnJacobian ) {
-            attr( result, "jacobian" ) <- jac
-         }
-      }
-      class( result ) <- c( "margEff.censReg", class( result ) )
-   } else {
-      result <- NULL
+   if( isPanel ) {
+      stop( "the margEff() method for objects of class 'censReg'",
+         " can not yet be used for panel data models" )
    }
+
+   sigma <- allPar[ "sigma" ]
+   beta <- allPar[ ! names( allPar ) %in% c( "sigma" ) ]
+   if( length( object$xMean ) != length( beta ) ){
+      print( beta )
+      print( object$xMean )
+      stop( "cannot calculate marginal effects due to an internal error:",
+               " please contact the maintainer of this package" )
+   }
+   xBeta <- crossprod( object$xMean, beta )
+   zRight <- ( object$right - xBeta ) / sigma
+   zLeft <- ( object$left - xBeta ) / sigma
+   result <- beta[ ! names( beta ) %in% c( "(Intercept)" ) ] * 
+      ( pnorm( zRight ) - pnorm( zLeft ) )
+   names( result ) <- 
+      names( beta )[ ! names( beta ) %in% c( "(Intercept)" ) ]
+
+   if( calcVCov ){
+      # compute Jacobian matrix
+      jac <- matrix( 0, nrow = length( result ), ncol = length( allPar ) )
+      rownames( jac ) <- names( result )
+      colnames( jac ) <- names( allPar )
+      for( j in names( result ) ) {
+         for( k in names( allPar )[ -length( allPar ) ] ) {
+            jac[ j, k ] <- 
+               ( j == k ) * ( pnorm( zRight ) - pnorm( zLeft ) ) -
+               ( beta[ j ] * object$xMean[ j ] / sigma ) *
+               ( dnorm( zRight ) - dnorm( zLeft ) )
+         }
+         jac[ j, "sigma"] <- 0
+         if( is.finite( object$right ) ) {
+            jac[ j, "sigma"] <- jac[ j, "sigma"] - ( beta[ j ] / sigma ) *
+               dnorm( zRight ) * zRight
+         }
+         if( is.finite( object$left ) ) {
+            jac[ j, "sigma"] <- jac[ j, "sigma"] + ( beta[ j ] / sigma ) *
+               dnorm( zLeft ) * zLeft
+         }
+      }
+      attr( result, "vcov" ) <- 
+         jac %*% vcov( object, logSigma = FALSE ) %*% t( jac )
+      if( returnJacobian ) {
+         attr( result, "jacobian" ) <- jac
+      }
+   }
+   class( result ) <- c( "margEff.censReg", class( result ) )
+ 
    return( result )
 }

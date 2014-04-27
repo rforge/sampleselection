2fbf9fe0d5b39aaf3ca94ca33f3e0a1223ed5dbd
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2014-04-27 15:35:16 +0000

    added tests for ML estimations of models based on the data set Mroz87 (basically copied the tests for 2-step estimations)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1541 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/sampleSelection/tests/heckit.R
+++ pkg/sampleSelection/tests/heckit.R
@@ -3,6 +3,7 @@ data( "Mroz87" )
 options( digits = 3 )
 
 ## Greene( 2003 ): example 22.8, page 786
+## 2-step estimation
 Mroz87$kids  <- ( Mroz87$kids5 + Mroz87$kids618 > 0 )
 greene <- heckit( lfp ~ age + I( age^2 ) + faminc + kids + educ,
    wage ~ exper + I( exper^2 ) + educ + city, Mroz87 )
@@ -28,7 +29,38 @@ print( model.matrix( greene, part = "outcome" ) )
 print( model.matrix( greene, part = "selection" ) )
 print( model.frame( greene ) )
 
+# ML estimation
+greeneMl <- selection( lfp ~ age + I( age^2 ) + faminc + kids + educ,
+   wage ~ exper + I( exper^2 ) + educ + city, Mroz87 )
+print( greeneMl )
+print(summary( greeneMl ))
+print( coef( greeneMl ) )
+print( coef( greeneMl, part = "outcome" ) )
+print( coef( summary( greeneMl ) ) )
+print( coef( summary( greeneMl ), part = "outcome" ) )
+print( vcov( greeneMl ) )
+print( vcov( greeneMl, part = "outcome" ) )
+all.equal( nobs( greene ), nobs( greeneMl ) )
+all.equal( nObs( greene ), nObs( greeneMl ) )
+fitted( greeneMl, part = "outcome" )
+fitted( greeneMl, part = "selection" )
+residuals( greeneMl, part = "outcome" )
+residuals( greeneMl, part = "selection", type = "response" )
+all.equal( model.matrix( greene, part = "outcome" )[,-6],
+   model.matrix( greeneMl, part = "outcome" ),
+   check.attributes = FALSE )
+model.matrix( greeneMl, part = "outcome" )
+all.equal( model.matrix( greene, part = "selection" ),
+   model.matrix( greeneMl, part = "selection" ),
+   check.attributes = FALSE )
+model.matrix( greeneMl, part = "selection" )
+all.equal( model.frame( greene )[,-c(2,12)], model.frame( greeneMl ),
+   check.attributes = FALSE )
+model.frame( greeneMl )
+
+
 ## Wooldridge( 2003 ): example 17.5, page 590
+## 2-step estimation
 data( "Mroz87" )
 wooldridge <- heckit( lfp ~ nwifeinc + educ + exper + I( exper^2 ) + age +
    kids5 + kids618, log( wage ) ~ educ + exper + I( exper^2 ), Mroz87 )
@@ -53,6 +85,36 @@ round( residuals( wooldridge, part = "selection", type = "response" ), digits = 
 print( model.matrix( wooldridge, part = "outcome" ) )
 print( model.matrix( wooldridge, part = "selection" ) )
 print( model.frame( wooldridge ) )
+
+# ML estimation
+wooldridgeMl <- selection( lfp ~ nwifeinc + educ + exper + I( exper^2 ) + age +
+      kids5 + kids618, log( wage ) ~ educ + exper + I( exper^2 ), Mroz87 )
+print( wooldridgeMl )
+print( summary( wooldridgeMl ) )
+print( coef( wooldridgeMl ) )
+print( coef( wooldridgeMl, part = "outcome" ) )
+print( coef( summary( wooldridgeMl ) ) )
+print( coef( summary( wooldridgeMl ), part = "outcome" ) )
+print( vcov( wooldridgeMl ) )
+print( vcov( wooldridgeMl, part = "outcome" ) )
+nobs( wooldridgeMl )
+nObs( wooldridgeMl )
+round( fitted( wooldridgeMl, part = "outcome" ), digits = 3 )
+round( fitted( wooldridgeMl, part = "selection" ), digits = 3 )
+round( residuals( wooldridgeMl, part = "outcome" ), digits = 3 )
+round( residuals( wooldridgeMl, part = "selection", type = "response" ), digits = 3 )
+all.equal( model.matrix( wooldridge, part = "outcome" )[,-5],
+   model.matrix( wooldridgeMl, part = "outcome" ),
+   check.attributes = FALSE )
+model.matrix( wooldridgeMl, part = "outcome" )
+all.equal( model.matrix( wooldridge, part = "selection" ),
+   model.matrix( wooldridgeMl, part = "selection" ),
+   check.attributes = FALSE )
+model.matrix( wooldridgeMl, part = "selection" )
+all.equal( model.frame( wooldridge )[,-c(2,11)], model.frame( wooldridgeMl ),
+   check.attributes = FALSE )
+print( model.frame( wooldridgeMl ) )
+
 
 ## Tobit 5 Example from the selection paper
 library( "mvtnorm" )

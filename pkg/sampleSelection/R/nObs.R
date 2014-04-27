93ac0d05b84c05b0b64a25cf52e1fbb5f51d06d7
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2014-04-27 06:11:12 +0000

    added nobs() methods for probit and selection models (equal to the corresponding nObs() methods)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1536 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/sampleSelection/R/nObs.R
+++ pkg/sampleSelection/R/nObs.R
@@ -2,6 +2,14 @@ nObs.probit <- function(x, ...) {
    return( x$param$nObs )
 }
 
+nobs.probit <- function( object, ... ) {
+   return( nObs.probit( object, ... ) )
+}
+
 nObs.selection <- function(x, ...) {
    return( x$param$nObs )
+}
+
+nobs.selection <- function( object, ... ) {
+   return( nObs.selection( object, ... ) )
 }

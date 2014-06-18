75fc9903a0313a4f13d85ea76d801194d2824ecc
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2014-06-18 07:31:31 +0000

    predict.selection(): model matrices and coefficients are now only obtained if they are needed for the specific predictions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1581 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/sampleSelection/R/predict.selection.R
+++ pkg/sampleSelection/R/predict.selection.R
@@ -12,71 +12,83 @@ predict.selection <- function( object, newdata = NULL,
    
    if( is.null( newdata ) ) {
       # regressor matrix for the selection equation
-      mXSelection <- model.matrix( object, part = "selection" )
+      if( part == "selection" || type == "conditional" ) {
+         mXSelection <- model.matrix( object, part = "selection" )
+      }
       
       # regressor matrix for the outcome equation
-      mXOutcome <- model.matrix( object, part = "outcome" )
-
-      # remove inverse Mills ratio
-      if( object$method == "2step" ) {
-         if( object$tobitType == 2 ) {
-            mXOutcome <- mXOutcome[ , -ncol( mXOutcome ) ]
-         } else if( object$tobitType == 5 ) {
-            for( i in 1:2 ) {
-               mXOutcome[[i]] <- mXOutcome[[i]][ , -ncol( mXOutcome[[i]] ) ]
+      if( part == "outcome" ) {
+         mXOutcome <- model.matrix( object, part = "outcome" )
+      
+         # remove inverse Mills ratio
+         if( object$method == "2step" ) {
+            if( object$tobitType == 2 ) {
+               mXOutcome <- mXOutcome[ , -ncol( mXOutcome ) ]
+            } else if( object$tobitType == 5 ) {
+               for( i in 1:2 ) {
+                  mXOutcome[[i]] <- mXOutcome[[i]][ , -ncol( mXOutcome[[i]] ) ]
+               }
             }
          }
       }
       
    } else {
       # regressor matrix for the selection equation
-      tempS <- eval( object$call$selection )
-      formS <- as.formula( tempS )
-      mfS <- model.frame( formS, data = newdata, na.action = na.pass )
-      mXSelection <- model.matrix( formS, mfS )
+      if( part == "selection" || type == "conditional" ) {
+         tempS <- eval( object$call$selection )
+         formS <- as.formula( tempS )
+         mfS <- model.frame( formS, data = newdata, na.action = na.pass )
+         mXSelection <- model.matrix( formS, mfS )
+      }
       
       # regressor matrix for the outcome equation
-      tempO <- eval( object$call$outcome )
-      if( object$tobitType == 2 ) {
-         formO <- as.formula( tempO )
-         mfO <- model.frame( formO, data = newdata, na.action = na.pass )
-         mXOutcome <- model.matrix( formO, mfO )
-      } else if( object$tobitType == 5 ) {
-         mXOutcome <- list()
-         for( i in 1:2 ) {
-            formO <- as.formula( tempO[[i]] )
+      if( part == "outcome" ) {
+         tempO <- eval( object$call$outcome )
+         if( object$tobitType == 2 ) {
+            formO <- as.formula( tempO )
             mfO <- model.frame( formO, data = newdata, na.action = na.pass )
-            mXOutcome[[i]] <- model.matrix( formO, mfO )
+            mXOutcome <- model.matrix( formO, mfO )
+         } else if( object$tobitType == 5 ) {
+            mXOutcome <- list()
+            for( i in 1:2 ) {
+               formO <- as.formula( tempO[[i]] )
+               mfO <- model.frame( formO, data = newdata, na.action = na.pass )
+               mXOutcome[[i]] <- model.matrix( formO, mfO )
+            }
          }
       }
    }
-
-   # estimated parameters
-   vIndexBetaS <- object$param$index$betaS
-   vBetaS <- coef( object )[ vIndexBetaS ]
    
-   if( object$tobitType == 2 ) {
-      vIndexBetaO <- object$param$index$betaO
-      vBetaO <- coef( object )[ vIndexBetaO ]
-      dLambda <- coef( object )[ "rho" ] * coef( object )[ "sigma" ]
-      # remove coefficient of inverse Mills ratio
-      if( object$method == "2step" ) {
-         vBetaO <- vBetaO[ names( vBetaO ) != "invMillsRatio" ]
-      }
-   } else if( object$tobitType == 5 ) {
-      vIndexBetaO <- list()
-      vIndexBetaO[[1]] <- object$param$index$betaO1
-      vIndexBetaO[[2]] <- object$param$index$betaO2
-      vBetaO <- list()
-      dLambda <- list()
-      for( i in 1:2 ) {
-         vBetaO[[ i ]] <- coef( object )[ vIndexBetaO[[ i ]] ]
-         dLambda[[ i ]] <- coef( object )[ paste0( "rho", i ) ] *
-            coef( object )[ paste0( "sigma", i ) ]
+   # estimated parameters
+   if( part == "selection" || type == "conditional" ) {
+      vIndexBetaS <- object$param$index$betaS
+      vBetaS <- coef( object )[ vIndexBetaS ]
+   }
+   
+   if( part == "outcome" ) {
+      if( object$tobitType == 2 ) {
+         vIndexBetaO <- object$param$index$betaO
+         vBetaO <- coef( object )[ vIndexBetaO ]
+         dLambda <- coef( object )[ "rho" ] * coef( object )[ "sigma" ]
          # remove coefficient of inverse Mills ratio
          if( object$method == "2step" ) {
-            vBetaO[[ i ]] <- vBetaO[[ i ]][
-               names( vBetaO[[ i ]] ) != "invMillsRatio" ]
+            vBetaO <- vBetaO[ names( vBetaO ) != "invMillsRatio" ]
+         }
+      } else if( object$tobitType == 5 ) {
+         vIndexBetaO <- list()
+         vIndexBetaO[[1]] <- object$param$index$betaO1
+         vIndexBetaO[[2]] <- object$param$index$betaO2
+         vBetaO <- list()
+         dLambda <- list()
+         for( i in 1:2 ) {
+            vBetaO[[ i ]] <- coef( object )[ vIndexBetaO[[ i ]] ]
+            dLambda[[ i ]] <- coef( object )[ paste0( "rho", i ) ] *
+               coef( object )[ paste0( "sigma", i ) ]
+            # remove coefficient of inverse Mills ratio
+            if( object$method == "2step" ) {
+               vBetaO[[ i ]] <- vBetaO[[ i ]][
+                  names( vBetaO[[ i ]] ) != "invMillsRatio" ]
+            }
          }
       }
    }

2848d12a1cdc33572621792d0f58385cc0ec0855
otoomet <otoomet@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2015-08-30 20:36:02 +0000

    stdEr.intReg now works with boundaries=TRUE argument.      stdEr for sigma now correct (was bug in maxLik) tests/general.R now works correctly tests/intReg renamed to test/intreg_privateTest
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1658 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/intReg/R/stdEr.intReg.R
+++ pkg/intReg/R/stdEr.intReg.R
@@ -1,6 +1,6 @@
-stdEr.intReg <- function( x, boundaries=FALSE, ... ) {
+stdEr.intReg <- function( x, boundaries=FALSE, ...) {
    ## stdEr method.  By default, ignore the fixed boundaries.
-   stde <- NextMethod("stdEr", x, ...)
+   stde <- NextMethod("stdEr", ...)
    if(!boundaries) {
       i <- rep(TRUE, nParam(x))
       i[x$param$index$boundary] <- FALSE

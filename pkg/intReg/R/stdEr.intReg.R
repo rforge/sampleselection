34701032de3680d6251b1eb4b4bf45ac5c0c0aa6
otoomet <otoomet@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2016-08-21 19:28:09 +0000

    intReg.Rd: wording improved
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1725 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/intReg/R/stdEr.intReg.R
+++ pkg/intReg/R/stdEr.intReg.R
@@ -1,5 +1,6 @@
 stdEr.intReg <- function( x, boundaries=FALSE, ...) {
    ## stdEr method.  By default, ignore the fixed boundaries.
+   cat("stdEr.intReg..")
    stde <- NextMethod("stdEr", ...)
    if(!boundaries) {
       i <- rep(TRUE, nParam(x))
@@ -7,5 +8,6 @@ stdEr.intReg <- function( x, boundaries=FALSE, ...) {
       stde <- stde[i]
    }
    class( stde ) <- c( "stdEr.intReg", class(stde) )
+   cat("\n")
    return( stde )
 }

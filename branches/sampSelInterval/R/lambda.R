bf0f5b07695c8246eb7397910b5408275ee17096
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2016-09-20 11:06:34 +0000

    removed /branches/sampSelInterval/ because this branch should be based on the latest version of the sampleSelection package (/pkg/sampleSelection/), i.e. after merging /branches/treatReg/ to /pkg/sampleSelection/
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1729 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- branches/sampSelInterval/R/lambda.R
+++
@@ -1,6 +0,0 @@
-### inv mills ratio.
-### Should accept all kind of values, including far off range.
-lambda <- function(x) {
-   as.vector(ifelse(x > -30, dnorm(x)/pnorm(x), -x))
-                           # can we prove it?
-}

451652c7bb8178115c2e4aca51e9de24a3b84798
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2016-09-20 18:27:47 +0000

    added tests of the new function intervalfit()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1732 d36ae590-6c38-44c5-9ba5-7217e93607d8

---
+++ branches/sampSelInterval/tests/interval.R
@@ -0,0 +1,39 @@
+library( "sampleSelection" )
+library( "mvtnorm" )
+options( digits = 2 )
+
+nObs <- 300
+betaS <- c( 1, 1, -1 )
+betaO <- c( 10, 4 )
+rho <- 0.4
+sigma2 <- 5
+bound <- c(-Inf,5,15,Inf)
+
+
+set.seed(123)
+dat <- data.frame( x1 = rnorm( nObs ), x2 = rnorm( nObs ) )
+Sigma <- matrix( c( 1, rho*sigma2, rho*sigma2, sigma2^2 ), nrow = 2 )
+eps <- rmvnorm( nObs, sigma = Sigma )
+dat$epsS <- eps[,1]
+dat$epsO <- eps[,2]
+dat$yS <- with( dat, betaS[1] + betaS[2] * x1 + betaS[3] * x2 + epsS ) > 0
+# table(dat$yS)
+dat$yOu <- with( dat, betaO[1] + betaO[2] * x1 + epsO )
+dat$yOu[ !dat$yS ] <- NA
+hist( dat$yOu )
+dat$yO <- cut( dat$yOu, bound )
+
+YS <- dat$yS
+XS <- cbind( 1, dat$x1, dat$x2 )
+YO <- as.numeric( dat$yO ) - 1
+XO <- cbind( 1, dat$x1 )
+
+start <- c( betaS, betaO, rho, sigma2 )
+
+res <- sampleSelection:::intervalfit( YS, XS, YO, XO, boundaries = bound,
+   start = start, printLevel = 1 )
+   
+print( res )
+print( round( coef( res ), 2 ) )
+print( round( coef( summary( res ) ), 2 ) )
+maxLik:::summary.maxLik( res )

02f3cee41bff50ad78d0f7bd012e7e133a96e218
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2017-01-14 18:11:27 +0000

    changed the line breaks and removed unnecessary parenthesis in the code that calculates the partial derivatives wrt sigma_2 so that it is easier (for humans) to read the code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1789 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- branches/sampSelInterval/R/intervalfit.R
+++ branches/sampSelInterval/R/intervalfit.R
@@ -120,18 +120,23 @@ intervalfit <- function(YS, XS, YO, XO, boundaries, start, AnalyticGrad,
             if( YS[i] == 0 ) {
                grad[i, iSigma2] <- 0
             } else {
-               grad[i, iSigma2] <- (pnorm((XS.b[i] - rho * 
-                     ((boundaries[ YO[i] + 1 ] - XO.b[i])/sigma2))/
-                     (sqrt(1-rho^2))) * dnorm((boundaries[ YO[i] + 1 ] -
-                           XO.b[i])/sigma2) * ((XO[i]-boundaries[ YO[i] + 1 ])/
-                                 (sigma2^2)) - pnorm((XS.b[i] - rho * 
-                                       ((boundaries[ YO[i] ] - XO.b[i])/sigma2))/
-                                       (sqrt(1-rho^2))) * dnorm((boundaries[ YO[i] ] - XO.b[i])/
-                                             sigma2) * ((XO[i]-boundaries[ YO[i] ])/(sigma2^2)) )/
-                  (pmvnorm( upper = c( ( boundaries[ YO[i] + 1 ] - XO.b[i] ) /
-                        sigma2, XS.b[i] ), sigma = Sigma ) -
-                        pmvnorm( upper = c( ( boundaries[ YO[i] ] - XO.b[i] ) /
-                              sigma2, XS.b[i] ), sigma = Sigma ) )
+               grad[i, iSigma2] <- (
+                  pnorm( ( XS.b[i] - rho *
+                     ( ( boundaries[ YO[i] + 1 ] - XO.b[i] ) / sigma2 ) ) /
+                        sqrt( 1 - rho^2 ) ) *
+                  dnorm( ( boundaries[ YO[i] + 1 ] - XO.b[i] ) / sigma2 ) *
+                  ( ( XO[i] - boundaries[ YO[i] + 1 ] ) / sigma2^2 ) -
+                  pnorm( ( XS.b[i] - rho *
+                     ( ( boundaries[ YO[i] ] - XO.b[i] ) / sigma2 ) ) /
+                        sqrt( 1 - rho^2 ) ) *
+                  dnorm( ( boundaries[ YO[i] ] - XO.b[i] ) / sigma2 ) *
+                  ( ( XO[i] - boundaries[ YO[i] ] ) / sigma2^2 ) ) /
+                  ( pmvnorm( upper =
+                     c( ( boundaries[ YO[i] + 1 ] - XO.b[i] ) / sigma2, XS.b[i] ),
+                     sigma = Sigma ) -
+                  pmvnorm( upper =
+                     c( ( boundaries[ YO[i] ] - XO.b[i] ) / sigma2, XS.b[i] ),
+                     sigma = Sigma ) )
                # if( is.na( grad[i, iSigma2] ) ) browser()
             }
          }

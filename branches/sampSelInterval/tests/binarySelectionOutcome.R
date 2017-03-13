3177b984932ece559ea93fba431bb855ebb19a2c
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2017-03-13 09:25:50 +0000

    added tests for estimating selection models with binary dependent variables and only an intercept as explanatory variable
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1882 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- branches/sampSelInterval/tests/binarySelectionOutcome.R
+++ branches/sampSelInterval/tests/binarySelectionOutcome.R
@@ -136,3 +136,14 @@ print(table(simDat$ys, simDat$yo, exclude=NULL))
 ssLN <- selection( ys ~ xs, yo ~ xo, data = simDat, steptol = 1e-12 )
 print(summary(ssLN))
 all.equal(ssL,ssLN)
+
+# only intercept in outcome equation
+try( ssNo <- selection( ys ~ xs, yo ~ 1, data = simDat ) )
+
+# only intercept in selection equation
+try( ssNs <- selection( ys ~ 1, yo ~ xo, data = simDat,
+   start = c( 0, -1, 2, 0.5 ) ) )
+
+# only intercept in both equations
+try( ssNso <- selection( ys ~ 1, yo ~ 1, data = simDat,
+   start = c( 0, -1, 0.5 ) ) )

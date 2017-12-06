66cff89470676bb5d3ec833d22afc56301ad9d8a
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2017-12-06 13:02:54 +0000

    added further tests with incorrectly specified arguments of selection()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1947 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/sampleSelection/tests/fail.R
+++ pkg/sampleSelection/tests/fail.R
@@ -1,20 +1,37 @@
 library( "sampleSelection" )
 
+try( selection( s ~ z1, y ~ x1, type = "w" ) )
+
 # tobit 2
 
 try( selection( s ~ z1, y ~ x1 ) )
 
+try( selection( s ~ z1, y ~ x1, type = "2" ) )
+
 try( selection( "s ~ z1", y ~ x1 ) )
+
+try( selection( "s ~ z1", y ~ x1, type = "2" ) )
 
 try( selection( ~ z1, y ~ x1 ) )
 
+try( selection( ~ z1, y ~ x1, type = "2" ) )
+
 try( selection( s ~ z1, "y ~ x1" ) )
 
+try( selection( s ~ z1, "y ~ x1", type = "2" ) )
+
 try( selection( s ~ z1, ~ x1 ) )
+
+try( selection( s ~ z1, ~ x1, type = "2" ) )
+
+try( selection( s ~ z1, list( y ~ x1, y ~ x1 ), type = "2" ) )
+
 
 # tobit 5
 
 try( selection( s ~ z1, list( y1 ~ x1, y2 ~ x1 ) ) )
+
+try( selection( s ~ z1, list( y1 ~ x1, y2 ~ x1 ), type = "5" ) )
 
 try( selection( "s ~ z1", list( y1 ~ x1, y2 ~ x1 ) ) )
 
@@ -28,3 +45,6 @@ try( selection( s ~ z1, list( y1 ~ x1, "y2 ~ x1" ) ) )
 
 try( selection( s ~ z1, list( y1 ~ x1, ~ x1 ) ) )
 
+try( selection( s ~ z1, y1 ~ x1, type = "5" ) )
+
+try( selection( s ~ z1, "y1 ~ x1", type = "5" ) )

be64dc3b13a3e4cf15380d96622e87b0a495498e
arne <arne@d36ae590-6c38-44c5-9ba5-7217e93607d8>
2017-12-06 13:26:44 +0000

    added tests with incorrectly specified arguments of selection( , type = "treatment" )
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sampleselection@1948 d36ae590-6c38-44c5-9ba5-7217e93607d8

--- pkg/sampleSelection/tests/fail.R
+++ pkg/sampleSelection/tests/fail.R
@@ -2,6 +2,7 @@ library( "sampleSelection" )
 
 try( selection( s ~ z1, y ~ x1, type = "w" ) )
 
+
 # tobit 2
 
 try( selection( s ~ z1, y ~ x1 ) )
@@ -48,3 +49,21 @@ try( selection( s ~ z1, list( y1 ~ x1, ~ x1 ) ) )
 try( selection( s ~ z1, y1 ~ x1, type = "5" ) )
 
 try( selection( s ~ z1, "y1 ~ x1", type = "5" ) )
+
+
+# treatment effects models
+
+try( selection( s ~ z1, y1 ~ s + x1, type = "treatment" ) )
+
+try( selection( s ~ z1, y1 ~ x1, type = "treatment" ) )
+
+try( selection( "s ~ z1", y1 ~ s + x1, type = "treatment" ) )
+
+try( selection(  ~ z1, y1 ~ s + x1, type = "treatment" ) )
+
+try( selection( s ~ z1, "y1 ~ s + x1", type = "treatment" ) )
+
+try( selection( s ~ z1, ~ s + x1, type = "treatment" ) )
+
+try( selection( s ~ z1, list( y1 ~ s + x1, y1 ~ s + x1 ), type = "treatment" ) )
+
